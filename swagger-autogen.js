22261491adbc7f0cc4cb1b1847913c05b486bf20
herbertmoroni <herbertmoroni@hotmail.com>
2025-02-11 15:07:40 -0700

    update swagger

--- swagger-autogen.js
+++ swagger-autogen.js
@@ -1,13 +1,19 @@
+const dotenv = require('dotenv');
 const swaggerAutogen = require('swagger-autogen')();
+
+// Load environment variables
+dotenv.config();
 
 const doc = {
   info: {
     title: 'Contacts API',
     description: 'API for managing contacts'
   },
-  host: process.env.NODE_ENV === 'production' ? 'https://name-api-whdx.onrender.com' : 'localhost:3000',
+  host: process.env.NODE_ENV === 'production' ? 'name-api-whdx.onrender.com' : 'localhost:3000',
   schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http'],
 };
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 const outputFile = './swagger.json';
 
